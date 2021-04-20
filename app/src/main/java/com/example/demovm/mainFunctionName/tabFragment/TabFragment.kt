2598d8f2c0975227bcdc7fd4ec4dfb39345ab8a5
li-cheng <li-cheng@mail.chungyo.net>
2021-04-20 14:54:49 +0800

    新增 MPAndroidChart 折線圖功能 https://www.tpisoftware.com/tpu/articleDetails/1844 / multidex > 65536 設定

--- app/src/main/java/com/example/demovm/mainFunctionName/tabFragment/TabFragment.kt
+++ app/src/main/java/com/example/demovm/mainFunctionName/tabFragment/TabFragment.kt
@@ -3,11 +3,13 @@ package com.example.demovm.mainFunctionName.tabFragment
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -20,6 +22,11 @@ import com.example.demovm.mainFunctionName.FunctionNameMainActivityViewModel
 import com.example.demovm.mainFunctionName.number.three.ThreeFragment
 import com.example.demovm.mainFunctionName.tabFragment.item.ItemAdapter
 import com.example.demovm.mainFunctionName.tabFragment.item.ItemDetail
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import timber.log.Timber
 
 
@@ -45,6 +52,7 @@ class TabFragment : BaseDaggerFragment() {
     val activityViewModel by activityViewModels<FunctionNameMainActivityViewModel> { viewModelFactory }
     override lateinit var binding: TabFragmentBinding
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -70,6 +78,7 @@ class TabFragment : BaseDaggerFragment() {
         Log.i(TAG, "ViewPager headerBack: " + activityViewModel.headerBack.value)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun initobserver() {
         viewModel.itemListEvent.observe(this, Observer {
             Log.i(TAG, "initobserver: " + viewModel.itemList.size)
@@ -155,6 +164,54 @@ class TabFragment : BaseDaggerFragment() {
                                 Timber.d("ActivityNotFoundException: $e")
                             }
                         }
+                    }
+
+                    "six" ->{
+                        // Entry(Float, Float)
+                        val dataOne : ArrayList<Entry> = arrayListOf(Entry(1f,5f),Entry(2f,7f),Entry(3f,3f))
+                        val dataTwo : ArrayList<Entry> = arrayListOf(Entry(1f,2f),Entry(2f,4f),Entry(3f,6f))
+
+                        binding.lineChart.apply {
+                            // axisRight, axisLeft 左右邊的Y軸設定
+                            this.axisRight.isEnabled = false // 設定顯示右邊Y軸
+
+                            this.xAxis.let { //設定 X軸
+                                it.position = XAxis.XAxisPosition.BOTTOM //將x軸表示字移到底下
+                                it.setLabelCount(10, false) //設定X軸上要有幾個標籤
+                            }
+                            val set1 = LineDataSet(dataOne, "one").apply {
+                                this.mode = LineDataSet.Mode.LINEAR //類型為折線
+                                this.color = context.getColor(R.color.aqua) //線的顏色
+                                this.lineWidth = 1.5f //線寬
+                                this.setDrawCircles(true) //不顯示相應座標點的小圓圈(預設顯示)
+                                this.setDrawValues(false) //不顯示座標點對應Y軸的數字(預設顯示)
+                            }
+                            val set2 = LineDataSet(dataTwo, "two").apply {
+                                this.mode = LineDataSet.Mode.LINEAR //類型為折線
+                                this.color = context.getColor(R.color.brown) //線的顏色
+                                this.lineWidth = 1.5f //線寬
+                                this.setDrawCircles(true) //不顯示相應座標點的小圓圈(預設顯示)
+                                this.setDrawValues(false) //不顯示座標點對應Y軸的數字(預設顯示)
+                            }
+
+                            this.description.apply { // 設定圖表內標籤
+                                this.text = "表內標籤"
+                                this.textSize = 14f
+                                this.textColor = context.getColor(R.color.red)
+                                this.setPosition(800f,100f) //顯示位置座標 (預設右下方)
+                            }
+
+                            this.setDrawBorders(true)//顯示邊框 (預設不顯示)
+                            this.setBorderColor(context.getColor(R.color.dark_salmon))
+                            this.setBorderWidth(5f)
+
+                            this.data = LineData(set1,set2)
+                            this.invalidate() //繪製圖表
+                        }
+
+                    }
+                    "seven" ->{
+
                     }
                 }
             }
